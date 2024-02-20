f72437f2cf525de1efbee233a748ef55b8b0672f
caowei49 <88067457+caowei49@users.noreply.github.com>
2024-02-20 09:06:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ cp -r $elkhuaweiplugin_dir/HuaweiDialGrpc /usr/elk/
 
 cat >> /usr/elk/logstash-5.5.0/Gemfile << EOF
 gem "ruby-protocol-buffers", "1.6.1"
-gem "google-protobuf", "3.5.0.pre", :path => "/usr/elk/logstash-5.5.0/vendor/bundle/jruby/1.9/gems/google-protobuf-3.5.0.pre"
 gem "logstash-codec-hw-telemetry-gpb", :path => "/usr/elk/logstash-5.5.0/local-plugins/logstash-codec-telemetry-gpb"
-gem "logstash-codec-protobuf", :path => "/usr/elk/logstash-5.5.0/local-plugins/logstash-codec-protobuf"
 gem "logstash-input-unix-hw", :path => "/usr/elk/logstash-5.5.0/local-plugins/logstash-input-unix-hw"
 EOF
